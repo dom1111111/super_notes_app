9f82dac3489355891e336104edf5239166c0bc18
dom <domenic101@tuta.io>
2022-10-16 20:57:31 -0400

    added a function to better choose commands from input words, with more specific keyword requirements

--- superapp_MAIN.py
+++ superapp_MAIN.py
@@ -42,22 +42,41 @@ def tell_current_date():
     funcs.Output.user_message(message)
 
 #--------------------#
-# Voice commands
+# Voice command
 
-voice_command_map = {
-    ('make','create','new','note','page'):funcs.AppNotesTasks.create_full_note,
-    ('make','create','new','task'):lambda a : print('dummy command! +', a),
-    ('find', 'search', 'get', 'return', 'retrieve', 'show', 'note', 'notes', 'page'):funcs.AppNotesTasks.full_search,
-    ('get', 'show', 'return', 'display', 'read', 'last', 'recent', 'note', 'notes'):funcs.AppNotesTasks.get_recent_notes,
-    ('exit', 'bye', 'goodbye', 'program', 'application'):shutdown,
-    ('say', 'speak', 'tell', 'get', 'display', 'what', 'current', 'time', 'now'):tell_current_time,
-    ('say', 'speak', 'tell', 'get', 'display', 'what', 'current', 'date', 'today', 'todays', "today's"):tell_current_date
-    }
+def find_command_from_words(command_words:list):
+    # turn the command into a set of words
+    command_words = set(command_words)
+    # these are variables representing each keyword and their synonyms
+    ## they are True if any command words are present in their sets
+    get = not command_words.isdisjoint({'get', 'return', 'retrieve', 'show', 'display', 'read'})
+    search = not command_words.isdisjoint({'search', 'find', 'seek', 'look'})
+    create = not command_words.isdisjoint({'create','make','new'})
+    recent = not command_words.isdisjoint({'recent', 'latest', 'last'})
+    current = not command_words.isdisjoint({'current', 'present'})
+    today = not command_words.isdisjoint({'today', 'todays', "today's"})
+    note = not command_words.isdisjoint({'note', 'notes', 'page'})
+    task = not command_words.isdisjoint({'task', 'todo', 'to-do'})
+    exit = not command_words.isdisjoint({'exit', 'bye', 'goodbye'})
+    # a function to see if individual words are in the set
+    def is_word_in_command(word):
+        return True if word in command_words else False
+    # a list of commands and their keyword requirements
+    commands = [
+        (funcs.AppNotesTasks.create_full_note, (create and note)),
+        #(funcs.AppNotesTasks.create_full_task, (create and task)),
+        (funcs.AppNotesTasks.full_search, (search and note)),
+        (funcs.AppNotesTasks.get_recent_notes, (recent and note)),
+        (tell_current_time, ((current or is_word_in_command('now')) and is_word_in_command('time'))),
+        (tell_current_date, ((current or today) and is_word_in_command('date'))),
+        (shutdown, (exit))
+    ]
+    # now return the command whose requirement is met 
+    for command, requirement in commands:
+        if requirement:
+            return command
 
 def voice_command():
-    # while True:
-    #   while True:
-
     # 1) collect audio
     funcs.Output.play_tone(1)               # play tone 1 to let user know that the voice command recording is starting
     funcs.AudioRec.start()                  # start recording command audio
@@ -79,12 +98,13 @@ def voice_command():
     # 2) split the audio text into comand and content according to one of the split words
     try: # only do this if audio text is properly returned
         audio_text_words = audio_text.split()
+        audio_text_words_lower = audio_text.lower().split()
     except:
         return
-    split_words = ("content", "Content")
+    split_words = ("content", "stuff")
     ## only do this if a split word is present (not all commands require splitting for arguments)
     # finds the first instance of a split word - any words coming after are ignored for splitting 
-    for word in audio_text_words:
+    for word in audio_text_words_lower:
         if word in split_words:
             split_index = audio_text_words.index(word)
             ## seperate text into command and content
@@ -96,24 +116,9 @@ def voice_command():
             command_words = audio_text_words
             content = ''
     # 3) compare the command words against the map to see which command is the best match
-    command_likelyness = []
-    for keywords_tuple in voice_command_map.keys():
-        for word in command_words:
-            if word in keywords_tuple:
-                command = voice_command_map.get(keywords_tuple)
-                command_likelyness.append(command)
-    if command_likelyness:                  # only do this if list is not empty
-        command = mode(command_likelyness)  # get the command which shows up most often in the list (best match)
-    else:
-        funcs.Output.program_message('ERROR! Command not found')
-        funcs.Output.user_message("Sorry, I have no idea what you're talking about")
-        return
-    # 4) finally, execute the command
-    if content:
-        command(content)
-    else:
-        command()
-    return
+    command = find_command_from_words(command_words)
+    # 4) finally, execute the command with content as an argument, but only if there is content 
+    command(content) if content else command()
 
 #--------------------#
 # Main
